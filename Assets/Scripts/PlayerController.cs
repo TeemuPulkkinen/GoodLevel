c1b55ff385ff5f0e60f071db2690681a4452c9d0
Teemu <pulkkinen.teemu@gmail.com>
2020-03-15 10:44:23 +0200

    Better enemies and players

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,43 +5,6 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     
-    // walking speed of the player
-    public float walkSpeed;
     
-    // Camera distance in z from player
-    public float cameraDistZ = 6;
 
-    // Rigidbody
-    private Rigidbody rb;
-
-    // Collider
-    private Collider col;
-
-    //size of the player
-    private Vector3 size;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        col = GetComponent<Collider>();
-
-        size = col.bounds.size;
-
-        CameraFollowPlayer();
-    }
-
-    void FixedUpdate()
-    {
-        
-    }
-
-    private void CameraFollowPlayer()
-    {
-        Vector3 cameraPosition = Camera.main.transform.position;
-
-        cameraPosition.z = transform.position.z - cameraDistZ;
-
-        Camera.main.transform.position = cameraPosition;
-    }
 }
