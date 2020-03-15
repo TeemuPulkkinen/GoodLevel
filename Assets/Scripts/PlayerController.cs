55c9f5140c070bd7068fed1c7b10bd4a82c5599a
Teemu <pulkkinen.teemu@gmail.com>
2020-03-15 13:19:39 +0200

    Added more walls.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    
+    [SerializeField]
+    private Rigidbody playerBody;
+
+    private Vector3 inputVector;
+
+    private void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * 10f, playerBody.velocity.y, Input.GetAxis("Vertical") * 10f);
+        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+    }
+
+    private void FixedUpdate()
+    {
+        playerBody.velocity = inputVector;
+    }
 
 }
